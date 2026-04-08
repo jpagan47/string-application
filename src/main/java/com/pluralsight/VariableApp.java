f45e5f65ce66ddf36d308e51c2a1366498e343de
josepagan <josepaganrnj@outlook.com>
2026-04-08 15:09:57 -0400

    Initial Commmit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        //My Favorite color Declaration
+        String favoriteColor = "Blue";
+        System.out.println("My favorite Color is " + favoriteColor);
+        //The Year i started class in Year Up
+        int yearStartedClass = 2026;
+        System.out.println("The year i started year up is " + yearStartedClass);
+        // My middle initail is
+        char myMiddleInitial = 'A';
+        System.out.println("My middle initial is " + myMiddleInitial);
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
