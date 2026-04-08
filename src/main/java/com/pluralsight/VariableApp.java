d12e8d882aabf7a2af9cfdd6851040cbcd4ac790
josepagan <josepaganrnj@outlook.com>
2026-04-08 15:28:02 -0400

    Step 1 Complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -7,11 +7,23 @@ public class VariableApp {
         System.out.println("My favorite Color is " + favoriteColor);
         //The Year i started class in Year Up
         int yearStartedClass = 2026;
-        System.out.println("The year i started year up is " + yearStartedClass);
+        System.out.println("The year I started year up is " + yearStartedClass);
         // My middle initail is
         char myMiddleInitial = 'A';
         System.out.println("My middle initial is " + myMiddleInitial);
-
+        //Wether I have pets or not
+        boolean doIHavePets = true;
+        System.out.println("Survey says do you have any pets? " + doIHavePets);
+        //Nice Message to pring out
+        String myNiceMessage = "Hey dont punch your monitor out of Frustration";
+        System.out.println("My Message To my Teamamates is; " + myNiceMessage);
+        // Declaring and Initializing Variables for Number of days in the week
+        char numOfDaysInTheWeek = '7';
+        System.out.println("There are " + numOfDaysInTheWeek + " Days in a week");
+        //The price of coffee in a expensive coffe shop
+        double priceOfCoffe = 4.99;
+        System.out.println(" The price of coffee aT A VERY EXPENSIVE shop is; " + priceOfCoffe);
+        //
 
 
 
