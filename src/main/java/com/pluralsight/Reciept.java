dc4c648f9bb63a7a1d22fd1d4f98634f9706e60b
josepagan <josepaganrnj@outlook.com>
2026-04-08 16:56:50 -0400

    Step 3 Complete

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+        String fooOrdered = "Sushi";
+        double priceOfFood = 7.99;
+        int amountofOrders = 1;
+        System.out.println("Your ordered " + fooOrdered );
+        System.out.println("Your total orders are.." + amountofOrders);
+        System.out.println("Your total is $" + priceOfFood);
+    }
+
+
+
+
+
+
+
+}
