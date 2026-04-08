01d8e7f64045609ed2745716b7e192a1c5e1b54c
josepagan <josepaganrnj@outlook.com>
2026-04-08 16:46:15 -0400

    Step 2 Complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -15,15 +15,24 @@ public class VariableApp {
         boolean doIHavePets = true;
         System.out.println("Survey says do you have any pets? " + doIHavePets);
         //Nice Message to pring out
-        String myNiceMessage = "Hey dont punch your monitor out of Frustration";
+        String myNiceMessage = "'Hey dont punch your monitor out of Frustration'";
         System.out.println("My Message To my Teamamates is; " + myNiceMessage);
+        //End of step #1
         // Declaring and Initializing Variables for Number of days in the week
         char numOfDaysInTheWeek = '7';
         System.out.println("There are " + numOfDaysInTheWeek + " Days in a week");
         //The price of coffee in a expensive coffe shop
         double priceOfCoffe = 4.99;
-        System.out.println(" The price of coffee aT A VERY EXPENSIVE shop is; " + priceOfCoffe);
-        //
+        System.out.println("The price of coffee aT A VERY EXPENSIVE shop is; " + priceOfCoffe);
+        //My favore letter is
+        char myFavoriteLetterIs = 'A';
+        System.out.println("My favorite letter is " + myFavoriteLetterIs);
+        //The weather wether its raining or not
+        boolean isRainingOutside = false;
+        System.out.println("Is it raining outside? " + isRainingOutside);
+        //End of Step #2
+        //End of Coding
+
 
 
 
