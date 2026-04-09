5afb75e05bda98ab6276ee9edb04f777f46b4ac8
josepagan <josepaganrnj@outlook.com>
2026-04-09 11:49:45 -0400

    Added White Space for User Readability

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -5,32 +5,43 @@ public class VariableApp {
         //My Favorite color Declaration
         String favoriteColor = "Blue";
         System.out.println("My favorite Color is " + favoriteColor);
-        //The Year i started class in Year Up
+
+        //The Year I started class in Year Up
         int yearStartedClass = 2026;
         System.out.println("The year I started year up is " + yearStartedClass);
-        // My middle initail is
+
+        // My middle initial is
         char myMiddleInitial = 'A';
         System.out.println("My middle initial is " + myMiddleInitial);
+
         //Wether I have pets or not
         boolean doIHavePets = true;
         System.out.println("Survey says do you have any pets? " + doIHavePets);
-        //Nice Message to pring out
-        String myNiceMessage = "'Hey dont punch your monitor out of Frustration'";
-        System.out.println("My Message To my Teamamates is; " + myNiceMessage);
+
+        //Nice Message to print out
+        String myNiceMessage = "'Hey don't punch your monitor out of Frustration'";
+        System.out.println("My Message To my Teammate's is; " + myNiceMessage);
+
         //End of step #1
+
         // Declaring and Initializing Variables for Number of days in the week
         char numOfDaysInTheWeek = '7';
         System.out.println("There are " + numOfDaysInTheWeek + " Days in a week");
-        //The price of coffee in a expensive coffe shop
+
+        //The price of coffee in an expensive coffe shop
         double priceOfCoffe = 4.99;
         System.out.println("The price of coffee aT A VERY EXPENSIVE shop is; " + priceOfCoffe);
-        //My favore letter is
+
+        //My favorite letter is
         char myFavoriteLetterIs = 'A';
         System.out.println("My favorite letter is " + myFavoriteLetterIs);
-        //The weather wether its raining or not
+
+        //The weather wether it's raining or not
         boolean isRainingOutside = false;
         System.out.println("Is it raining outside? " + isRainingOutside);
+
         //End of Step #2
+
         //End of Coding
 
 
